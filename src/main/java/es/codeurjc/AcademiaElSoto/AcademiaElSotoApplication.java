dfae930f8ea229b56972f3e7a84c22346d2eaf37
MaxVintilaPop <ma.vintila.2018@alumnos.urjc.es>
2026-02-15 19:18:43 +0100

    Adapted for spring

---
+++ src/main/java/es/codeurjc/AcademiaElSoto/AcademiaElSotoApplication.java
@@ -0,0 +1,13 @@
+package es.codeurjc.AcademiaElSoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaElSotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaElSotoApplication.class, args);
+	}
+
+}
