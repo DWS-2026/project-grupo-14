765df13cc8f28d1adb1e74dded56110c8b874ed1
IEC12 <nachoenco@gmail.com>
2026-02-17 10:41:07 +0100

    clases java por entidad

---
+++ src/main/java/es/codeurjc/AcademiaElSoto/Curso.java
@@ -0,0 +1,8 @@
+package es.codeurjc.AcademiaElSoto;
+
+public class Curso {
+    private String profesor;
+    private String nombreCurso;
+    private int precio;
+    private String descripcion;
+}
