de8c7a4b773b2c0b8104335de44d65e49eab9480
IEC12 <nachoenco@gmail.com>
2026-02-20 11:45:18 +0100

    clases.java

--- src/main/java/es/codeurjc/AcademiaElSoto/Carrito.java
+++ src/main/java/es/codeurjc/AcademiaElSoto/Carrito.java
@@ -1,5 +1,29 @@
 package es.codeurjc.AcademiaElSoto;
 
 public class Carrito {
+    private String producto;
+    private int precio;
+
+    public Carrito (String producto, int precio){
+        this.precio=precio;
+        this.producto=producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
     
 }
