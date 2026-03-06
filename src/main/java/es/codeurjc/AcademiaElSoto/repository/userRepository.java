2cfe095773b376865033dd7ead8ab8922a5ff8e3
IEC12 <nachoenco@gmail.com>
2026-03-06 12:17:59 +0100

    Controllers

--- src/main/java/es/codeurjc/AcademiaElSoto/repository/userRepository.java
+++ src/main/java/es/codeurjc/AcademiaElSoto/repository/userRepository.java
@@ -3,7 +3,7 @@ package es.codeurjc.AcademiaElSoto.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import es.codeurjc.AcademiaElSoto.model.Usuario;
 
-public interface UserRepository extends JpaRepository<Usuario, Long> {
+public interface userRepository extends JpaRepository<Usuario, Long> {
 
     Usuario findByEmail(String email);
 
