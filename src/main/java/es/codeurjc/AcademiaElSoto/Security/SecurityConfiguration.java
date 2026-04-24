538296167d9a9b971cf09f542242a5ecd9429308
MaxVintilaPop <ma.vintila.2018@alumnos.urjc.es>
2026-04-24 11:40:54 +0200

    Fixed some filenames and structure

--- src/main/java/es/codeurjc/AcademiaElSoto/Security/SecurityConfiguration.java
+++ src/main/java/es/codeurjc/AcademiaElSoto/Security/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package es.codeurjc.AcademiaElSoto.Security;
+package es.codeurjc.AcademiaElSoto.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
--- src/main/java/es/codeurjc/AcademiaElSoto/controller/WebController.java
+++ src/main/java/es/codeurjc/AcademiaElSoto/controller/WebController.java
@@ -1,4 +1,4 @@
-package es.codeurjc.AcademiaElSoto;
+package es.codeurjc.AcademiaElSoto.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
